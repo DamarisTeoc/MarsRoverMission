3b97f0d0e67c87a9541988483c1d62773a959ea0
DamarisTeoc <teoc.918@gmail.com>
2025-04-21 18:31:57 +0200

    movevectors and readme

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ import { RoverState, Command } from "./domain/types";
 
 const initial: RoverState = { position: { x: 0, y: 0 }, direction: "N" };
 
+const obstacles = [
+    { x: 2, y: 2 },
+    { x: 5, y: 5 },
+    { x: 1, y: 0 }
+];
+
 const [, , cmdString = ""] = process.argv;
 if (!cmdString) {
   console.error("Uso: node dist/index.js <comandos>");
@@ -12,7 +18,7 @@ if (!cmdString) {
 
 const commands = cmdString.toUpperCase().split("") as Command[];
 
-const rover = new Rover(initial, 200);
+const rover = new Rover(initial, 200, obstacles);
 const result = rover.execute(commands);
 
 console.log(JSON.stringify(result, null, 2));
